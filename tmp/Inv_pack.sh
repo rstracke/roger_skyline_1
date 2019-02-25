6c5148ab619531c0bb28fccc5d71d51441ffbf77
Victor <vic.baykov89@gmail.com>
2019-02-26 02:15:24 +0300

    RS1

--- tmp/Inv_pack.sh
+++
@@ -1,9 +0,0 @@
-	echo -en "${RED}Protection against INVALID PACKETS${NORMAL}\n"
-	iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
-	echo -en "${GREEN}\u2713 SET\n"
-	echo -en "${RED}Blocking NOT SYN new PACKETS${NORMAL}\n"
-	iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
-	echo -en "${GREEN}\u2713 SET\n"
-	echo -en "${RED}Blocking flood by SYN${NORMAL}\n"
-	iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
-	echo -en "${GREEN}\u2713 SET\n"
