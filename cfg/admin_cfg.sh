85a30299fdbad6faff1dcb213e5dd172a6252cfb
Robin potter Stracke <rstracke@e4r9p2.21-school.ru>
2019-02-27 20:27:58 +0300

    CFG

--- cfg/admin_cfg.sh
+++ cfg/admin_cfg.sh
@@ -154,8 +154,6 @@ ssl_install() {
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 cp res/self-signed.conf /etc/nginx/snippets/self-signed.conf
 cp res/default /etc/nginx/sites-enabled/default
-ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
-ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
 }
 #==================================================================================================
 deploy() {
