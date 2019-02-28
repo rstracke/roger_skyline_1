3213d634664c8c218948365d39d812b8b1ab4c48
Victor <vic.baykov89@gmail.com>
2019-02-28 20:24:36 +0300

    CFG

--- cfg/admin_cfg.sh
+++ cfg/admin_cfg.sh
@@ -1,7 +1,7 @@
 source ./utils/colors.sh
 source ./utils/str_processing.sh
 #==========================UPDATE, UPGRADE, INSTALL================================================
-PACKAGES=(sudo nginx net-tools ufw portsentry mailutils)
+PACKAGES=(sudo net-tools ufw portsentry nginx mailutils)
 
 update_packages() {
 	echo -en "\n${RED}${BGGREEN}APT update & upgrade${NORMAL}\n"
