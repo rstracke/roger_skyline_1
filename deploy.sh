918ca8ab4076c23985ca0f827db2cb421e4a4d62
Robin potter Stracke <rstracke@e4r9p2.21-school.ru>
2019-03-01 15:19:16 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 apt install unzip dialog openssh-server -y
 wget https://github.com/rstracke/roger_skyline_1/archive/master.zip
 unzip master.zip
-cp -r roger_skyline_1-master/* root/rs
 mkdir /root/rs
-cp .bashrc /root/rs/tmp/.bashrctmp
-cd /root/rs
-cp res/.bashrc /root/.bashrc
-cp /lib/systemd/system/getty@.service /root/rs/tmp/getty@.service
-cp res/getty@.service /lib/systemd/system/getty@.service
+cp -r roger_skyline_1-master/* /root/rs
 ./main.sh
