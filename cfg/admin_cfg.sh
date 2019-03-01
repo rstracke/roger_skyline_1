0a707cbc85efc9ee1f69a2f4ebcfde38b6df8245
Robin potter Stracke <rstracke@e4r9p4.21-school.ru>
2019-03-01 17:46:46 +0300

    scan

--- cfg/admin_cfg.sh
+++ cfg/admin_cfg.sh
@@ -189,13 +189,14 @@ deploy() {
 		flag=1
 		while [ $flag -eq 1 ]
 		do
-		if [ $res -eq 0 ]
-		then
-			echo -en "It seems you have no authorized keys in ${BOLD}${GREEN}~/.ssh ${NORMAL} directory.\n Perform ssh-keygen on your client. Then copy your id_rsa using ${BOLD}${GREEN}ssh-copy-id -i <your_.ssh>/<your_key> <user_name>@<host_IP>${NORMAL}"
-			read -p "Press any key. And restart your machine"
-		else
-			flag=0
-		fi
+			if [ $res -eq 0 ]
+			then
+				echo -en "It seems you have no authorized keys in ${BOLD}${GREEN}~/.ssh ${NORMAL} directory.\n Perform ssh-keygen on your client. Then copy your id_rsa using ${BOLD}${GREEN}ssh-copy-id -i <your_.ssh>/<your_key> <user_name>@<host_IP>${NORMAL}"
+				read -p "Press any key. And restart your machine"
+			else
+				flag=0
+			fi
+		done
 		install_packages
 		echo -en "################${GREEN}Making user sudoer${NORMAL}################\n"
 		sudo_setup victor
